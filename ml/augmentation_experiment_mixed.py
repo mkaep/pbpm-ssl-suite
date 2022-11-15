53e0cb27e30e78f263cd9d70ed7e11012d0a9439
Martin Käppel <martin.kaeppel@uni-bayreuth.de>
2022-11-15 12:44:30 +0100

    Add new experiment

--- ml/augmentation_experiment_mixed.py
+++ ml/augmentation_experiment_mixed.py
@@ -3,22 +3,24 @@ from ml.persistence import json
 
 if __name__ == '__main__':
     datasets = [
-        model.Dataset('Helpdesk', r'D:\PBPM_Approaches\experiment\data\Helpdesk.xes'),
-        model.Dataset('Sepsis', r'D:\PBPM_Approaches\experiment\data\Sepsis.xes'),
-        model.Dataset('BPIC13', r'D:\PBPM_Approaches\experiment\data\BPI_Challenge_2013_closed_problems.xes'),
-        #model.Dataset('BPIC12', r'D:\PBPM_Approaches\experiment\data\BPI_Challenge_2012.xes'),
-        model.Dataset('BPIC15_1', r'D:\PBPM_Approaches\experiment\data\BPIC15_1.xes')
+        model.Dataset('Helpdesk', r'/home/ai4-admin/event_logs/Helpdesk.xes'),
+        model.Dataset('Sepsis', r'/home/ai4-admin/event_logs/Sepsis.xes'),
+        model.Dataset('BPIC13', r'/home/ai4-admin/event_logs/BPI_Challenge_2013_closed_problems.xes'),
+        model.Dataset('BPIC13', r'/home/ai4-admin/event_logs/BPI_Challenge_2013_incidents.xes'),
+        model.Dataset('BPIC15_1', r'/home/ai4-admin/event_logs/BPIC15_1.xes'),
+        model.Dataset('BPIC12', r'/home/ai4-admin/event_logs/BPI_Challenge_2012.xes'),
+        model.Dataset('NASA', r'/home/ai4-admin/event_logs/NASA.xes')
     ]
 
     approaches = [
-        model.Approach('Buksh', 'process-transformer', 'D:\\PBPM_Approaches\\processtransformer\\main.py', {
+        model.Approach('Buksh', 'process-transformer', '/home/ai4-admin/workspace/pbpm-processtransformer/main.py', {
             'task': 'next_activity',
             'epochs': 10,
             'batch_size': 12,
             'learning_rate': 0.001,
             'gpu': 0,
         }),
-        model.Approach('Pasquadibisceglie', 'pasquadibisceglie', 'D:\\PBPM_Approaches\\pasquadibisceglie\\main.py', {
+        model.Approach('Pasquadibisceglie', 'pasquadibisceglie', '/home/ai4-admin/workspace/pbpm-pasquadibisceglie/main.py', {
             'batch_size': 128,
             'epochs': 500,
             'n_layers': 2,
@@ -26,7 +28,7 @@ if __name__ == '__main__':
             'validation_split': 0.2,
             'patience': 6
         }),
-        model.Approach('Camargo', 'camargo', 'D:/PBPM_Approaches/camargo/main.py', {
+        model.Approach('Camargo', 'camargo', '/home/ai4-admin/workspace/pbpm-camargo/main.py', {
             'n_gram_size': 5,
             'model_type': 'shared_categorical',
             'l_size': 100,
@@ -39,13 +41,45 @@ if __name__ == '__main__':
             'dense_act': None,
             'rp_sim': 0.85
         }),
-        model.Approach('Theis', 'theis-pydream', 'D:/PBPM_Approaches/theis-pydream/main.py', {
+        model.Approach('Theis', 'theis-pydream', '/home/ai4-admin/workspace/pbpm-theis/main.py', {
             'seed': 1,
             'epochs': 100,
             'batch_size': 64,
             'dropout_rate': 0.2,
             'eval_size': 0.1,
             'activation_function': 'relu'
+        }),
+        model.Approach('Tax', 'tax', '/home/ai4-admin/workspace/pbpm-tax/main.py', {
+            'prediction_task': "ACT_TIME",
+            'epochs': 500,
+            'validation_split': 0.2,
+            'learning_rate': 0.002,
+            'schedule_decay': 0.004,
+            'clipvalue': 3,
+            'patience': 10,
+            'prediction_size': 3
+        }),
+        model.Approach('Khan', 'khan', '/home/ai4-admin/workspace/pbpm-khan/main.py', {
+            'iterations': 20000,
+            'hidden_controller_dim': 100,
+            'use_emb': False,
+            'use_mem': True,
+            'decoder_mode': True,
+            'dual_controller': True,
+            'write_protect': True,
+            'attend_dim': 0,
+            'batch_size': 1,
+            'sequence_max_length': 100,
+            'words_count': 5,
+            'word_size': 20,
+            'read_heads': 1
+        }),
+        model.Approach('Mauro', 'mauro', '/home/ai4-admin/workspace/pbpm-mauro/main.py', {
+            'iterations': 20,
+            'model_type': 'ACT',
+            'epochs': 200,
+            'validation_split': 0.2,
+            'patience': 20
         }),
     ]
 
@@ -68,26 +102,26 @@ if __name__ == '__main__':
                                                                   'duration_tolerance': 0.2})
                        ]
 
-    # todo abbruchkriterium fuer single und parallel swap, wenn es nicht möglich ist, macht es keinen Sinn es immer weiter zu probieren
     augmentation_strategies_config = [
         model.AugmentationStrategyConfig(1, 'mixed', 42, augmentor_names, 1.2, True),
         model.AugmentationStrategyConfig(2, 'mixed', 42, augmentor_names, 1.4, True),
         model.AugmentationStrategyConfig(3, 'mixed', 42, augmentor_names, 1.6, True),
-        model.AugmentationStrategyConfig(3, 'mixed', 42, augmentor_names, 2, True)
+        model.AugmentationStrategyConfig(4, 'mixed', 42, augmentor_names, 2, True),
+        model.AugmentationStrategyConfig(5, 'mixed', 42, augmentor_names, 2.4, True),
+        model.AugmentationStrategyConfig(6, 'mixed', 42, augmentor_names, 2.6, True),
+        model.AugmentationStrategyConfig(7, 'mixed', 42, augmentor_names, 3, True)
     ]
 
-    experiment = model.AugmentationExperiment('compStudy', 'data', 'runs',
-                                              'D:\\PBPM_Approaches\\experiment\\evaluation_10', datasets, approaches,
+    experiment = model.AugmentationExperiment('aug_study', 'data', '/home/ai4-admin/runs',
+                                              '/home/ai4-admin/ev', datasets, approaches,
                                               splitter_configuration, 2, augmentation_strategies_config)
 
-    exporter = json.JsonExperimentExporter(r'/experiments/exp_small.json')
+    exporter = json.JsonExperimentExporter(r'/home/ai4-admin/runs/exp_small.json')
     exporter.save(experiment)
 
-    importer = json.JsonExperimentImporter(r'/experiments/exp_small.json')
+    importer = json.JsonExperimentImporter(r'/home/ai4-admin/runs/exp_small.json')
     loaded_experiment = importer.load()
-    print(loaded_experiment)
-    print(isinstance(loaded_experiment, model.AugmentationExperiment))
-    print(isinstance(loaded_experiment, model.Experiment))
+
 
 
 
